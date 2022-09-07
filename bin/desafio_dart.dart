f3a936f74b1bce29241b0b9bb6793282e90c617d
MizzaelCosta <mizzaelcosta+github@gmail.com>
2022-09-07 19:17:51 -0300

    create menu

--- bin/desafio_dart.dart
+++ bin/desafio_dart.dart
@@ -1,3 +1,50 @@
+import 'dart:convert';
+import 'dart:io';
+
 void main() {
-  print('hello world');
+  bool exitMenu = true;
+
+  do {
+    print('''
+        CADASTRO 
+        (1) Cadastrar nova Empresa
+        (2) Buscar Empresa por CNPJ
+        (3) Buscar Empresa por Sócio
+        (4) Listar Empresas
+        (5) Excluir Empresa
+        (6) Sair
+        ''');
+    String inputMenu = stdin.readLineSync(encoding: utf8)!;
+    switch (inputMenu) {
+      case '1':
+        {
+          print('Cadastrar nova Empresa');
+          break;
+        }
+      case '2':
+        {
+          print('Buscar Empresa por CNPJ');
+          break;
+        }
+      case '3':
+        {
+          print('Buscar Empresa por Sócio');
+          break;
+        }
+      case '4':
+        {
+          print('Listar Empresas');
+          break;
+        }
+      case '5':
+        {
+          print('Excluir Empresa');
+          break;
+        }
+      default:
+        {
+          (inputMenu == '6') ? exitMenu = false : print('Opção inválida.');
+        }
+    }
+  } while (exitMenu);
 }
