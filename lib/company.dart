575573db36f6612994e5029965414c787ea8ae4d
MizzaelCosta <mizzaelcosta+github@gmail.com>
2022-09-10 01:25:50 -0300

    add CPF/CNPJvalidator

--- lib/company.dart
+++ lib/company.dart
@@ -1,8 +1,10 @@
+import 'package:cpf_cnpj_validator/cnpj_validator.dart';
 import 'package:desafio_dart/address.dart';
 import 'package:desafio_dart/partner.dart';
 import 'dart:convert';
 import 'dart:io';
 import 'package:desafio_dart/id.dart';
+import 'package:cpf_cnpj_validator/cpf_validator.dart';
 
 class Company {
   late Id id;
@@ -23,7 +25,8 @@ class Company {
     print('Digite Nome Fantasia:');
     nomeFantasia = stdin.readLineSync(encoding: utf8)!;
     print('Digite CNPJ:');
-    cnpj = stdin.readLineSync(encoding: utf8)!;
+    String input;
+    cnpj = CNPJValidator.format(input = stdin.readLineSync(encoding: utf8)!);
     print('Digite Telefone:');
     telefone = stdin.readLineSync(encoding: utf8)!;
     print('Endereço Empresa...');
--- lib/partner.dart
+++ lib/partner.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:desafio_dart/address.dart';
+import 'package:cpf_cnpj_validator/cpf_validator.dart';
 
 class Partner {
   late String name;
@@ -8,8 +9,11 @@ class Partner {
   late Address address;
 
   Partner() {
+    print('Digite Nome Completo:');
     name = stdin.readLineSync(encoding: utf8)!;
-    cpf = stdin.readLineSync(encoding: utf8)!;
+    print('Digite CPF (Apenas NUmeros):');
+    String input;
+    cpf = CPFValidator.format(input = stdin.readLineSync(encoding: utf8)!);
     print('Endereço Sócio...');
     address = Address();
   }
